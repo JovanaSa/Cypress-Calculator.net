c45795e226ae464c3e1d017c7b36caffaf983ac9
unknown <jovana.savic.1401@gmail.com>
2024-06-07 16:38:07 +0200

    Home calculator div update

--- cypress/e2e/headeroutDiv.cy.js
+++ cypress/e2e/headeroutDiv.cy.js
@@ -7,24 +7,24 @@ describe('headerout', () => {
       logoImg: 'a > img',
       mortgageLink: '#hl1 > :nth-child(1) > a'
     };
-  
+    //odlazak na sajt pre svakog testa
     beforeEach(() => {
       cy.visit('https://www.calculator.net/');
       cy.get(selectors.logo).should('exist');
       cy.get(selectors.searchBox).should('exist');
     });
-  
+    //provera postojanja elemenata
     it('Element existence', () => {
       cy.get(selectors.headerOut).should('exist');
       cy.get(selectors.header).should('exist');
       cy.get(selectors.logo).should('exist');
       cy.get(selectors.logoImg).should('exist');
     });
-
+    //provera da li logo ima sliku
     it('Logo image', () => {
         cy.get('#logo img').should('exist').and('be.visible');
       });
-  
+    //provera funkcionalnosti logo slike da vraca na home page
     it('Logo navigation', () => {
       cy.get(selectors.mortgageLink).should('exist').click();
       cy.url().should('eq', 'https://www.calculator.net/mortgage-calculator.html');
--- cypress/e2e/homecaldivDiv.cy.js
+++ cypress/e2e/homecaldivDiv.cy.js
@@ -1,16 +1,46 @@
 describe('homecaldiv', () => {
     const selectors = {
       logo: '#logo',
+      searchBox: '#calcSearchTerm',
+      
     };
-  
+    //odlazak na sajt pre svakog testa
     beforeEach(() => {
       cy.visit('https://www.calculator.net/');
       cy.get(selectors.logo).should('exist');
       cy.get(selectors.searchBox).should('exist');
     });
-    
-    it('Logo image', () => {
-        cy.get('#logo img').should('exist').and('be.visible');
-      });
+  //provera postojanja elemenata
+    it('Element existence', () => {
+        cy.get('#homecaldiv').should('exist');
+        cy.get('#contentout').should('exist');
+        cy.get('table:first').should('exist');
+        cy.get('tbody:first').should('exist');
+        cy.get('#sciout').should('exist');
+        cy.get('#sciout > tbody > :nth-child(1) > td > :nth-child(1)').should('exist');
+        cy.get('#sciInPut').should('exist');
+        cy.get('#sciOutPut').should('exist');
+        //homefun div
+        cy.get('#homefunbtn').should('exist');
+        //prvi red
+        cy.get('#homefunbtn > :nth-child(1)').should('exist');
+        //kolone unutar prvog reda
+        cy.get('[onclick="r(\'sin\')"]').contains('sin').should('exist');
+        cy.get('[onclick="r(\'cos\')"]').contains('cos').should('exist');
+        cy.get('[onclick="r(\'tan\')"]').contains('tan').should('exist');
+        //radiobuttons
+        cy.get('.scird').should('exist');
+        cy.get('[for="scirdsettingd"]').should('exist');
+        cy.get('#scirdsettingd').should('exist');
+        cy.contains('Deg').should('exist');
+        cy.get('[for="scirdsettingr"]').should('exist');
+        cy.get('#scirdsettingr').should('exist');
+        cy.contains('Rad').should('exist');
+        //drugi red
+        cy.get('#homefunbtn > :nth-child(2)').should('exist');
+        //kolone unutar drugog reda
+        cy.get('[onclick="r(\'asin\')"]').contains('sin').should('exist');
+        cy.get('[onclick="r(\'asin\')"] > sup').contains('cos').should('exist');
+    });
 
 });
