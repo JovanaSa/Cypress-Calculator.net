58cf6fb89a7c2fde643643ef70e26e8a9b8abf2c
unknown <jovana.savic.1401@gmail.com>
2024-06-15 10:51:47 +0200

    existence

--- cypress/e2e/homecaldivDiv.cy.js
+++ cypress/e2e/homecaldivDiv.cy.js
@@ -89,7 +89,47 @@ describe('homecaldiv', () => {
         cy.get('[onclick="r(9)"]').contains('9').should('exist');
         cy.get('[onclick="r(\'+\')"]').contains('+').should('exist');
         cy.get('[onclick="r(\'bk\')"]').contains('Back').should('exist');
+        //drugi red
+        cy.get('[style="padding-top:3px;"] > :nth-child(2)').should('exist');
+        //kolone unutar drugog reda
+        cy.get('[onclick="r(4)"]').contains('4').should('exist');
+        cy.get('[onclick="r(5)"]').contains('5').should('exist');
+        cy.get('[onclick="r(6)"]').contains('6').should('exist');
+        cy.get('[onclick="r(\'-\')"]').contains('–').should('exist');
+        cy.get('[onclick="r(\'ans\')"]').contains('Ans').should('exist');
+        //treci red
+        cy.get('[style="padding-top:3px;"] > :nth-child(3)').should('exist');
+        //kolone unutar treceg reda
+        cy.get('[onclick="r(1)"]').contains('1').should('exist');
+        cy.get('[onclick="r(2)"]').contains('2').should('exist');
+        cy.get('[onclick="r(3)"]').contains('3').should('exist');
+        cy.get('[onclick="r(\'*\')"]').contains('×').should('exist');
+        cy.get('[onclick="r(\'M+\')"]').contains('M+').should('exist');
+        //cetvrti red
+        cy.get('[style="padding-top:3px;"] > :nth-child(4)').should('exist')
+        //kolone unutar cetvrtog reda
+        cy.get('[onclick="r(0)"]').contains('0').should('exist');
+        cy.get('[onclick="r(\'.\')"]').contains('.').should('exist');
+        cy.get('[onclick="r(\'EXP\')"]').contains('EXP').should('exist');
+        cy.get('[onclick="r(\'/\')"]').contains('/').should('exist');
+        cy.get('[onclick="r(\'M-\')"]').contains('M-').should('exist');
+        //peti red
+        cy.get('[style="padding-top:3px;"] > :nth-child(5)').should('exist');
+        //kolone unutar petog reda
+        cy.get('[onclick="r(\'+/-\')"]').contains('±').should('exist');
+        cy.get('[onclick="r(\'RND\')"]').contains('RND').should('exist');
+        cy.get('[onclick="r(\'C\')"]').contains('AC').should('exist');
+        cy.get('[onclick="r(\'=\')"]').contains('=').should('exist');
+        cy.get('#scimrc').contains('MR').should('exist');
         //druga tabela homecal>kraj
+        //homesch
+        cy.get('#homesch').should('exist');
+        cy.get('h1').contains('Free Online Calculators').should('exist');
+        cy.get('form > table').contains('Search').should('exist');
+        cy.get('#calcSearchTerm').should('exist');
+        cy.get('input').should('exist');
+        cy.get('#bluebtn').contains('Search').should('exist');
+        cy.get('#calcSearchOut').should('exist');
     });
     //provera funcionalnosti buttona
     it('Button functionallity', () => {
